6394f45932aa62ef36af5fd7f04c2286874d8195
Fırat Can Taş <firatcantass@gmail.com>
2024-05-17 16:17:28 +0300

    🌱 create no data message component

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,12 +1,11 @@
-/* eslint-disable react/prop-types */
 import styles from "./message.module.css";
 
-function Message({ message }) {
+const Message = ({ message }) => {
   return (
     <p className={styles.message}>
       <span role="img">👋</span> {message}
     </p>
   );
-}
+};
 
 export default Message;
