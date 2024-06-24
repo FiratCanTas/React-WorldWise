0f5c26ecbef14cf7392d9443946a024ba7aa4020
Fırat Can Taş <firatcantass@gmail.com>
2024-06-24 17:07:07 +0300

    🌱 apply getCity function

--- src/components/cityItem/CityItem.jsx
+++ src/components/cityItem/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import Flag from "../flag/Flag";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,15 +9,6 @@ const formatDate = (date) =>
     year: "numeric",
     weekday: "long",
   }).format(new Date(date));
-
-const flagEmojiToPNG = (flag) => {
-  const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-    .join("");
-  return (
-    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
-  );
-};
 
 const CityItem = ({ city }) => {
   const { cityName, emoji, date, id, position } = city;
@@ -27,7 +19,10 @@ const CityItem = ({ city }) => {
         className={styles.cityItem}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
-        <span className={styles.emoji}>{flagEmojiToPNG(emoji)}</span>
+        <span className={styles.emoji}>
+          {" "}
+          <Flag emoji={emoji} />
+        </span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
