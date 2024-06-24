ca81c6baa7e5f75e2ef00814d058d1c08fe1a995
Fırat Can Taş <firatcantass@gmail.com>
2024-06-24 17:06:00 +0300

    🌱 update import path

--- src/components/backButton/BackButton.jsx
+++ src/components/backButton/BackButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import Button from "../button/Button";
 
 const BackButton = () => {
   const navigate = useNavigate();
